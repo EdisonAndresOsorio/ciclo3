1ece03526600ab0638cff1bc1a9c3fbf5dcc0075
wilmarp <wilmarp@uninorte.edu.co>
2021-10-11 21:57:16 -0500

    Producto individual (Wilmar)

--- rest.py
+++ rest.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, jsonify
 #import productos
 from productos import getProductosList
+from  producto import getProducto
 
 
 
@@ -12,10 +13,20 @@ app.secret_key = os.urandom(24)
 def index():
     return render_template('index.html')
 
+@app.route('/producto', methods=['GET'])
+@app.route('/producto/', methods=['GET'])
+def producto():
+    return render_template('product.html')
+
 @app.route('/productos', methods=['GET','POST'])
 @app.route('/productos/', methods=['GET','POST'])
 def getProductos():
     return jsonify(getProductosList());
+
+@app.route('/obtenerProducto', methods=['GET','POST'])
+@app.route('/obtenerProducto/', methods=['GET','POST'])
+def obtenerProducto():
+    return jsonify(getProducto());
 
 if(__name__ == '__main__'):
     app.run(debug=True, port=8080)
