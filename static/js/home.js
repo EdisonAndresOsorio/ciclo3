16ace15aed3104ddc4c6affc29aabde62f720553
EdisonAndresOsorio <91963266+EdisonAndresOsorio@users.noreply.github.com>
2021-10-13 15:00:47 -0500

    update in style

--- static/js/home.js
+++ static/js/home.js
@@ -26,58 +26,56 @@ servicios = {
                 for(var i=0; i<data.length; i++){
 
                     newProducto = 
-                    '       <div id="carousel-example-generic'+i+'" class="carousel slide col-md-2" data-ride="carousel" data-interval="false">'+
-                    '		<h4>'+data[i].Nombre+'</h4> '+
-                    '		<span class="heart">Agregar a la lista de deseos'+
-                    '		<i class="'+(data[i].Deseado == "true" ? 'heart-on' : '')+' fa fa-heart"></i></span>'+
-                    '		<ol class="carousel-indicators">'+
-                    '			<li data-target="#carousel-example-generic'+i+'" data-slide-to="0" class="active"></li>'+
-                    '			<li data-target="#carousel-example-generic'+i+'" data-slide-to="1"></li>'+
-                    '			<li data-target="#carousel-example-generic'+i+'" data-slide-to="2"></li>'+
-                    '		</ol>'+
-                    '		    <div class="carousel-inner" role="listbox">';
+                    '<div id="carousel-example-generic'+i+'" class="carousel slide col-md-2" data-ride="carousel" data-interval="false">'+
+                    '   <div style="height:60px;"><h4>'+data[i].Nombre+'</h4></div>'+
+                    '   <span class="heart">Agregar a la lista de deseos'+
+                    '   <i class="'+(data[i].Deseado == "true" ? 'heart-on' : '')+' fa fa-heart"></i></span>'+
+                    '   <ol class="carousel-indicators">'+
+                    '       <li data-target="#carousel-example-generic'+i+'" data-slide-to="0" class="active"></li>'+
+                    '       <li data-target="#carousel-example-generic'+i+'" data-slide-to="1"></li>'+
+                    '       <li data-target="#carousel-example-generic'+i+'" data-slide-to="2"></li>'+
+                    '	</ol>'+
+                    '   <div class="carousel-inner" role="listbox">';
 
                     for (var j = 0; j<data[i].Imagenes.length; j++) {
 
-                        newProducto +=  '			    <div class="item '+ (j==0 ? "active" : "")+ '">'+
-                                        '			        <img src="'+data[i].Imagenes[j]+'" alt="...">'+
-                                        '			        <div class="carousel-caption"></div>'+
-                                        '			    </div>';							                                        
+                        newProducto +=  '<div class="item '+ (j==0 ? "active" : "")+ '">'+
+                                        '    <img src="'+data[i].Imagenes[j]+'" alt="...">'+
+                                        '    <div class="carousel-caption"></div>'+
+                                        '</div>';							                                        
                     }
                     
                     newProducto +=
-                    '		    </div>'+
-                    
-                    '		'+
-                    '		<a class="left carousel-control" href="#carousel-example-generic'+i+'" role="button" data-slide="prev">'+
-                    '			<span class="glyphicon glyphicon-chevron-left icon-prev" aria-hidden="true"></span>'+
-                    '			<span class="sr-only">Previous</span>'+
-                    '		</a>'+
-                    '		<a class="right carousel-control" href="#carousel-example-generic'+i+'" role="button" data-slide="next">'+
-                    '			<span class="glyphicon glyphicon-chevron-right icon-next" aria-hidden="true"></span>'+
-                    '			<span class="sr-only">Next</span>'+
-                    '    	</a>'+
-                    '		<form>'+
-                    '			<p class="clasificacion">'+
-                    '			  <input id="radio1" type="radio" name="estrellas" value="5">'+
-                    '			  <label for="radio1">★</label>'+
-                    '			  <input id="radio2" type="radio" name="estrellas" value="4">'+
-                    '			  <label for="radio2">★</label>'+
-                    '			  <input id="radio3" type="radio" name="estrellas" value="3">'+
-                    '			  <label for="radio3" class="votado">★</label>'+
-                    '			  <input id="radio4" type="radio" name="estrellas" value="2">'+
-                    '			  <label for="radio4" class="votado">★</label>'+
-                    '			  <input id="radio5" type="radio" name="estrellas" value="1">'+
-                    '			  <label for="radio5" class="votado">★</label>'+
-                    '			</p>'+
-                    '		  </form>'+
-                    '		<span class="s-precio">$ '+data[i].Precio+'</span>'+
-                    '		<a href="#" class="s-producto-comentarios">'+data[i].Comentarios+' comentarios</a>'+
-                    '       <div class="btn-del-edit-prod">'+
+                    '   </div>'+
+                    '   <a class="left carousel-control" href="#carousel-example-generic'+i+'" role="button" data-slide="prev">'+
+                    '   	<span class="glyphicon glyphicon-chevron-left icon-prev" aria-hidden="true"></span>'+
+                    '   	<span class="sr-only">Previous</span>'+
+                    '   </a>'+
+                    '   <a class="right carousel-control" href="#carousel-example-generic'+i+'" role="button" data-slide="next">'+
+                    '   	<span class="glyphicon glyphicon-chevron-right icon-next" aria-hidden="true"></span>'+
+                    '   	<span class="sr-only">Next</span>'+
+                    '   </a>'+
+                    '   <form>'+
+                    '       <p class="clasificacion">'+
+                    '           <input id="radio1" type="radio" name="estrellas" value="5">'+
+                    '           <label for="radio1">★</label>'+
+                    '           <input id="radio2" type="radio" name="estrellas" value="4">'+
+                    '           <label for="radio2">★</label>'+
+                    '           <input id="radio3" type="radio" name="estrellas" value="3">'+
+                    '           <label for="radio3" class="votado">★</label>'+
+                    '           <input id="radio4" type="radio" name="estrellas" value="2">'+
+                    '           <label for="radio4" class="votado">★</label>'+
+                    '           <input id="radio5" type="radio" name="estrellas" value="1">'+
+                    '           <label for="radio5" class="votado">★</label>'+
+                    '       </p>'+
+                    '   </form>'+
+                    '   <span class="s-precio">$ '+data[i].Precio+'</span>'+
+                    '   <a href="#" class="s-producto-comentarios">'+data[i].Comentarios+' comentarios</a>'+
+                    '   <div class="btn-del-edit-prod">'+
                     '       <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#formEditar">Editar</button>'+
                     '       <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#formEliminar">Elimiar</button>'+
-                    '        </div>'+
-                    '	</div>';
+                    '   </div>'+
+                    '</div>';
 
                     $("#list-productos").append(newProducto);   
                     if(data[i].Deseado == "true")                 
@@ -132,7 +130,7 @@ servicios = {
 
                     newProducto = 
                     '       <div id="carousel-example-generic'+i+'" class="carousel slide col-md-2" data-ride="carousel" data-interval="false">'+
-                    '		<h4>'+data[i].Nombre+'</h4> '+
+                    '		<div style="height:60px;"><h4>'+data[i].Nombre+'</h4></div>'+
                     '		<span>Agregar a la lista de deseos</span>'+
                     '		<a href="" class="heart '+(data[i].Deseado == "true" ? 'heart-on' : '')+' fa fa-heart"></a>'+
                     '		<ol class="carousel-indicators">'+
